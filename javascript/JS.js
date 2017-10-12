7ea3c3d0a1183992fe5d104d3ca64631318cdb14
MoralesEdgar <edgar.morales@tecmor.mx>
2017-10-12 02:52:25 -0500

    Add - Se agregan archivos de estilos y javascript.

---
+++ javascript/JS.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Edgar Morales on 12/10/2017.
+ */
+$(document).ready(function(){
+    $("textarea").addClass("materialize-textarea")
+    $("input[type='submit']").addClass("btn waves-effect waves-light")
+})
